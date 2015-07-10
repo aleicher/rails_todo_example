74cb5e2349533137b6166817b81d744ecdee855d
Andreas Leicher <andreas.leicher@mapcase.de>
2015-07-10 15:27:29 +0200

    todos can be edited
    - edit action is used to render the edit.html.erb page which contains
      the form to edit a todo
    - the form submits to update action (PUT/PATCH /todos/[:id])
    - note that the field done was not premitted, and changes to the done
      field are not updated unless they are allowed in the parameters

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,11 +17,18 @@ class TodosController < ApplicationController
     redirect_to @todo
   end
 
-  private
-  def todo_params
-    params.require(:todo).permit(:title, :description)
+  def edit
+    @todo = Todo.find(params[:id])
   end
 
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+  end
 
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :description, :done)
+  end
 
 end
