219661df9959c9da3e4877e1fbc60065a3b324ed
Andreas Leicher <andreas.leicher@mapcase.de>
2015-07-10 15:04:56 +0200

    new todos can be created
    - the form is shown on the todos/new page which renders the HTML to
      enter data for the todo
    - the form has to POST to the /todos endpoint
    - the POST to /todos is handled by the create method of the
      TodosController
    - the create method has to use strong parameters, see
      http://edgeguides.rubyonrails.org/action_controller_overview.html#parameters
      and introduces the private method todo_params

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,4 +10,18 @@ class TodosController < ApplicationController
   def new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.done = false
+    @todo.save
+    redirect_to @todo
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :description)
+  end
+
+
+
 end
